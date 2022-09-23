28a9c53a0fb0656a9bbfde3aa150ea2d0bcf8764
Martin Hart <martin.hart@etu.sorbonne-universite.fr>
2022-09-23 18:29:34 +0200

    Add: TME1 section 1.3

--- TME1/exo1/src/List.cpp
+++ TME1/exo1/src/List.cpp
@@ -1,71 +1,77 @@
+#include "List.h" // FAUTE : Oubli d'import du header."
+
+#include <iostream>
+#include <string>
 
 namespace pr {
 
 // ******************* Chainon
-Chainon::Chainon (const std::string & data, Chainon * next):data(data),next(next) {};
+Chainon::Chainon(const std::string& data, Chainon* next)
+    : data(data), next(next){};
 
 size_t Chainon::length() {
-	size_t len = 1;
-	if (next != nullptr) {
-		len += next->length();
-	}
-	return length();
+  size_t len = 1;
+  if (next != nullptr) {
+    len += next->length();
+  }
+  return len;  // FAUTE : Fonction récursive sans fin, ajout d'une condition de
+               // sortie.
 }
 
-void Chainon::print (std::ostream & os) {
-	os << data ;
-	if (next != nullptr) {
-		os << ", ";
-	}
-	next->print(os);
+// FAUTE ajout de const
+void Chainon::print(std::ostream& os) const {
+  os << data;
+  if (next != nullptr) {
+    os << ", ";
+  } else {  // FAUTE : Fonction récursive sans fin, ajout d'une condition de
+            // sortie.
+    return;
+  }
+  next->print(os);
 }
 
 // ******************  List
-const std::string & List::operator[] (size_t index) const  {
-	Chainon * it = tete;
-	for (size_t i=0; i < index ; i++) {
-		it = it->next;
-	}
-	return it->data;
+const std::string& List::operator[](size_t index) const {
+  Chainon* it = tete;
+  for (size_t i = 0; i < index; i++) {
+    it = it->next;
+  }
+  return it->data;
 }
 
-void List::push_back (const std::string& val) {
-	if (tete == nullptr) {
-		tete = new Chainon(val);
-	} else {
-		Chainon * fin = tete;
-		while (fin->next) {
-			fin = fin->next;
-		}
-		fin->next = new Chainon(val);
-	}
+void List::push_back(const std::string& val) {
+  if (tete == nullptr) {
+    tete = new Chainon(val);
+  } else {
+    Chainon* fin = tete;
+    while (fin->next) {
+      fin = fin->next;
+    }
+    fin->next = new Chainon(val);
+  }
 }
 
-void List::push_front (const std::string& val) {
-	tete = new Chainon(val,tete);
-}
+void List::push_front(const std::string& val) { tete = new Chainon(val, tete); }
 
-bool empty() {
-	return tete == nullptr;
-}
+// FAUTE : Oubli du prefix de la class List, rajout de 'List::'
+bool List::empty() { return tete == nullptr; }
 
 size_t List::size() const {
-	if (tete == nullptr) {
-		return 0;
-	} else {
-		return tete->length();
-	}
+  if (tete == nullptr) {
+    return 0;
+  } else {
+    return tete->length();
+  }
 }
 
-} // namespace pr
-
-std::ostream & operator<< (std::ostream & os, const pr::List & vec)
-{
-	os << "[";
-	if (vec.tete != nullptr) {
-		vec.tete->print (os) ;
-	}
-	os << "]";
-	return os;
+// FAUTE : Fonction en dehors du namespace pr, rajout dans le namespace pr.
+std::ostream& operator<<(std::ostream& os, const pr::List& vec) {
+  os << "[";
+  if (vec.tete != nullptr) {
+    vec.tete->print(os);
+  }
+  os << "]";
+  return os;
 }
 
+}  // namespace pr
--- TME1/exo1/src/main.cpp
+++ TME1/exo1/src/main.cpp
@@ -1,38 +1,43 @@
-#include "List.h"
-#include <string>
-#include <iostream>
 #include <cstring>
+#include <iostream>
+#include <string>
 
-int main () {
+#include "List.h"
 
-	std::string abc = "abc";
-	char * str = new char [3];
-	str[0] = 'a';
-	str[1] = 'b';
-	str[2] = 'c';
-	size_t i = 0;
+int main() {
+  std::string abc = "abc";
+  char *str = new char[4];
+  str[0] = 'a';
+  str[1] = 'b';
+  str[2] = 'c';
+  str[3] = '\0';  // FAUTE : Ajout d'un caractère de fin de chaine.
+  int i = 0;      // FAUTE : size_t overflow quand zéro est décrémenté.
 
-	if (! strcmp (str, abc.c_str())) {
-		std::cout << "Equal !";
-	}
+  if (!strcmp(str, abc.c_str())) {
+    std::cout << "Equal !";
+  }
 
-	pr::List list;
-	list.push_front(abc);
-	list.push_front(abc);
+  pr::List list;
+  list.push_front(abc);
+  list.push_front(abc);
 
-	std::cout << "Liste : " << list << std::endl;
-	std::cout << "Taille : " << list.size() << std::endl;
+  std::cout << "Liste : " << list << std::endl;
+  std::cout << "Taille : " << list.size() << std::endl;
 
-	// Affiche à l'envers
-	for (i= list.size() - 1 ; i >= 0 ; i--) {
-		std::cout << "elt " << i << ": " << list[i] << std::endl;
-	}
+  // Affiche à l'envers
+  for (i = list.size() - 1; i >= 0; i--) {
+    std::cout << "elt " << i << ": " << list[i] << std::endl;
+  }
 
-	// liberer les char de la chaine
-	for (char *cp = str ; *cp ; cp++) {
-		delete cp;
-	}
-	// et la chaine elle meme
-	delete str;
+  // FAUTE : La mémoire dynamique alloué a la chaine de caractère str
+  // est free deux fois.
+  /*
+    // libérer les char de la chaine
+    for (char *cp = str ; *cp ; cp++) {
+      delete cp;
+    }
+  */
 
+  // et la chaine elle même
+  delete[] str;
 }
